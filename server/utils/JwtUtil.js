eae5e140c8e708d0815f7ea0a1423e8aa328f0f2
tuannguyen1704 <142084430+tuannguyen1704@users.noreply.github.com>
2024-07-16 10:56:25 +0700

    Add files via upload

---
+++ server/utils/JwtUtil.js
@@ -0,0 +1,25 @@
+//CLI: npm install jsonwebtoken --save
+const jwt = require('jsonwebtoken');
+const MyConstants = require('./MyConstants');
+const JwtUtil = {
+  genToken(id) {
+    const token = jwt.sign({ id: id }, MyConstants.JWT_SECRET, { expiresIn: MyConstants.JWT_EXPIRES });
+    return token;
+  },
+  checkToken(req, res, next) {
+    const token = req.headers['x-access-token'] || req.headers['authorization'];
+    if (token) {
+      jwt.verify(token, MyConstants.JWT_SECRET, (err, decoded) => {
+        if (err) {
+          return res.json({ success: false, message: 'Token is not valid' });
+        } else {
+          req.decoded = decoded;
+          next();
+        }
+      });
+    } else {
+      return res.json({ success: false, message: 'Auth token is not supplied' });
+    }
+  }
+};
+module.exports = JwtUtil;
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'imagine.ekidkft.mongodb.net',
+  DB_USER: 'teemoanh2817',
+  DB_PASS: 'daohaitac123',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'imagin',
+  JWT_EXPIRES: '360000000', // in milliseconds
+  EMAIL_USER: 'teemoanh2817@gmail.com', // gmail service
+  EMAIL_PASS: 'vphk ngld atsq fcob'
+};
+module.exports = MyConstants;
